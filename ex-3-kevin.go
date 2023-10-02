19a7e564c90fb1801c8118788ddee4a822633e5b
marine-spaak <marine.spaak@gmail.com>
2023-10-02 12:12:00 +0200

    completion of the 3 exercises presented by Kevin

--- ex-3-kevin.go
+++ ex-3-kevin.go
@@ -9,44 +9,201 @@ import (
 // =======
 
 // Créer une liste chainée contenant un int comme valeur. Lui ajouter les méthodes suivantes
-// Ajout d'un noeud (donc nouvelle valeur)
-// Suppression d'un noeud (en fonction de sa valeur)
 // Obtenir le premier noeud
 // Obtenir le dernier noeud
+// Ajout d'un noeud (donc nouvelle valeur) - pas 2 noeuds identiques
+// Suppression d'un noeud (en fonction de sa valeur)
 
-func main() {
+// ========================================
+// DEFINITION DE "NODE" ET "LINKEDLIST"
+// ========================================
 
-	myString := "Hello;World;!"
-
-	result := ft_split(myString, ";")
-	fmt.Println(result)
-
+// Définition de la structure de "node" (un noeud de la liste chaînée)
+type node struct {
+	data int   // Valeur du noeud (un entier)
+	next *node // Pointeur vers le prochain noeud
 }
 
-func ft_split(stringToSplit string, separator string) []string {
+// Description de la structure de "linkedList" (une liste chaînée)
+type linkedList struct {
+	head   *node // Pointeur vers le premier noeud
+	length int   // Longueur de la liste
+}
 
-	// J'initialise le résultat
-	var resultArray []string
-	substring := ""
+// =================================
+// AFFICHER LES DONNEES DE LA LISTE
+// =================================
 
-	// Je parcours la chaine à séparer
-	for i := 0; i < len(stringToSplit); i++ {
-		// Si je tombe sur une lettre normale (pas le séparateur)
-		if string(stringToSplit[i]) != separator {
-			// Alors je l'ajoute à la sous-chaine que je suis en train de construire
-			substring = substring + string(stringToSplit[i])
+// Remarque : ici le récepteur est "linkedList" et non pas "*linkedList"
+// car on montre les données sans toucher à la structure de la liste
 
-			// Sinon (donc si je tombe sur le séparateur)
-		} else {
-			// J'ajoute la chaine actuelle au tableau de résultats
-			resultArray = append(resultArray, substring)
-			// Je remets la sous-chaine à zéro
-			substring = ""
+func (l linkedList) printListData() {
+	toPrint := l.head
+
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data) // Afficher un entier qui a pour valeur la donnée de toPrint
+		toPrint = toPrint.next          // Au prochain tour, on affichera le noeud suivant
+		l.length--                      // Je réduis la longueur qui tombera à 0 quand j'aurai parcouru toute la liste
+	}
+	fmt.Println("\n") // Saut de ligne
+}
+
+// ========================================
+// TROUVER LE PREMIER OU LE DERNIER NOEUD
+// ========================================
+
+// Obtenir le premier noeud
+func (l *linkedList) getFirstNode() *node {
+	return l.head
+}
+
+// Obtenir le dernier noeud
+func (l *linkedList) getLastNode() *node {
+	current := l.head
+	// Tant que je ne suis pas arrivée au bout, je récupère le suivant
+	for current.next != nil {
+		current = current.next
+	}
+	return current
+}
+
+// ==========================================
+// TROUVER UN NOEUD EN FONCTION DE SA VALEUR
+// ==========================================
+
+func (l *linkedList) findNodeByValue(v int) *node {
+	if l.head == nil {
+		// CAS 1 - LISTE VIDE (résultat nil)
+		return nil
+
+	} else {
+		// CAS 2 - LISTE NON VIDE
+		current := l.head         // Je pars du début
+		for current.next != nil { // Tant que je ne suis pas arrivée à la fin
+			if current.data == v { // Je check si la data du noeud vaut ce que je cherche
+				return current // Si c'est le cas, je renvoie le noeud actuel
+			} else {
+				current = current.next // Sinon, je passe au noeud suivant
+			}
+		}
+
+		// Si aucun n'a marché, je renvoie nil
+		return nil
+	}
+}
+
+// ==========================================
+// AJOUTER UN NOEUD (SANS CRÉER DE DOUBLONS)
+// ==========================================
+
+// METHODE PREPREND (ajouter un noeud au début de la liste)
+func (l *linkedList) prepend(n *node) {
+	if l.head == nil {
+		// CAS 1 - LISTE VIDE
+		l.head = n
+		l.length++
+	} else {
+		// CAS 2 - LISTE NON VIDE
+		// Je vérifie que la liste ne contient pas encore cette valeur
+		if l.findNodeByValue(n.data) == nil {
+			second := l.head     // Création d'une variable "second" recevant la head actuelle de la liste
+			l.head = n           // Le noeud reçu en argument devient la tête de la liste
+			l.head.next = second // Le noeud suivant sera "second" (l'ancienne tête de liste)
+			l.length++           // J'augmente la taille de la liste
 		}
 	}
-	// Il faut que j'ajoute le dernier bout de chaine (entre le dernier séparateur et la fin)
-	resultArray = append(resultArray, substring)
+}
 
-	// Je renvoie le tableau complet
-	return resultArray
+// METHODE APPEND (ajouter un noeud en fin de liste)
+func (l *linkedList) append(n *node) {
+	if l.head == nil {
+		// CAS 1 - LISTE VIDE
+		l.head = n
+		l.length++
+	} else {
+		// CAS 2 - LISTE NON VIDE
+		// J'obtiens le dernier noeud et je lui ajoute un suivant
+		last := l.getLastNode()
+		last.next = n
+		l.length++
+	}
+}
+
+// ============================================
+// SUPPRIMER UN NOEUD EN FONCTION DE SA VALEUR
+// ============================================
+
+func (l *linkedList) deleteNodeByValue(v int) {
+	// Le 1er noeud a-t-il la valeur v ?
+	if l.head.data == v {
+		// Si oui, je le retire et "head" devient le second noeud
+		l.head = l.head.next
+		l.length--
+	} else {
+		// Sinon...
+		previous := l.head     // Je définis que "previous" vaut le premier noeud
+		current := l.head.next // "current" vaut le second noeud
+
+		for current != nil {
+			if current.data == v {
+				// Si le noeud actuel a pour data la valeur que je cherche...
+				previous.next = current.next // Je fais en sorte que "previous.next" saute un noeud
+				current = current.next       // Je change le noeud actuel pour mettre le suivant à la place
+				l.length--
+			} else {
+				// Sinon je continue de parcourir la liste
+				previous = current
+				current = current.next
+			}
+		}
+	}
+}
+
+func main() {
+	myList := linkedList{}
+	node1 := &node{data: 49}
+	node2 := &node{data: 18}
+	node3 := &node{data: 16}
+	node4 := &node{data: 11}
+	node5 := &node{data: 7}
+	node6 := &node{data: 2}
+
+	myList.prepend(node1)
+	myList.prepend(node2)
+	myList.prepend(node3)
+
+	myList.append(node4)
+	myList.append(node5)
+	myList.append(node6)
+
+	myList.printListData()
+
+	// ======
+	// TESTS
+	// ======
+
+	// Trouver le premier et le dernier noeud
+	fmt.Println("Valeur du premier noeud : ", myList.getFirstNode().data)
+	fmt.Println("Valeur du dernier noeud : ", myList.getLastNode().data)
+
+	// Trouver le noeud qui a la valeur 49
+	fmt.Println("Le noeud ayant la valeur 49 : ", myList.findNodeByValue(49))
+	// Vérifier ce qui se passe quand on cherche une valeur inexistante dans la liste
+	fmt.Println("Chercher un noeud avec une valeur inexistante : ", myList.findNodeByValue(3))
+
+	// Supprimer le noeud qui a la valeur 49
+	myList.deleteNodeByValue(49)
+	fmt.Println("Ma liste en ayant retiré 49 :")
+	myList.printListData()
+
+	// Vérifier ce qui se passe quand on supprime une valeur inexistante
+	myList.deleteNodeByValue(3)
+	fmt.Println("Ma liste en ayant retiré une valeur inexistante :")
+	myList.printListData()
+
+	// Cas particuliers : si le noeud recherché est en 1ère ou dernière place
+	myList.deleteNodeByValue(16)
+	myList.deleteNodeByValue(2)
+	fmt.Println("Ma liste en ayant retiré le 1er et dernier noeud (par leurs valeurs) :")
+	myList.printListData()
 }
